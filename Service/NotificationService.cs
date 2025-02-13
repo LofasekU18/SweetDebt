ea88bbf76cbf22f85c81176a086e085234038d68
Lukáš Strnad <lstrnad13@gmail.com>
2025-02-13 22:30:27 +0100

    IConfiguration in Notification service

--- Service/NotificationService.cs
+++ Service/NotificationService.cs
@@ -7,23 +7,21 @@ namespace SweetDebt.Service
 {
     public class NotificationService
     {
-        private readonly IConfiguration _configuration;
         private readonly MimeMessage _email;
         private readonly SmtpClient _smtpClient;
-        public string Setting {  get; set; }
+        private readonly IConfiguration _configuration;
 
         public NotificationService(IConfiguration configuration)
         {
-            _configuration = configuration;
             _email = new MimeMessage();
             _smtpClient = new SmtpClient();
-            Setting =  _configuration.GetConnectionString("ToEmail");
+            _configuration = configuration;
         }
         public async Task SendNotificationAsync(MyTransaction transaction)
         {
             var email = new MimeMessage();
-            email.From.Add(new MailboxAddress($"Zloděj", "pokusomyl007@email.cz"));
-            email.To.Add(new MailboxAddress("Čudlík", $"{Setting}"));
+            email.From.Add(new MailboxAddress($"Zloděj", _configuration["AppSetting:NotificationEmail:FromEmail"]));
+            email.To.Add(new MailboxAddress("Čudlík", _configuration["AppSetting:NotificationEmail:ToEmail"]));
             email.Subject = "Pozor zloděj !";
 
             email.Body = new TextPart("plain")
@@ -34,8 +32,8 @@ namespace SweetDebt.Service
             using (var client = new SmtpClient())
             {
 
-                await client.ConnectAsync("smtp.seznam.cz", 465, MailKit.Security.SecureSocketOptions.SslOnConnect);
-                await client.AuthenticateAsync("pokusomyl007@email.cz", "FlorianovaLenicka1994");
+                await client.ConnectAsync(_configuration["AppSetting:NotificationEmail:SmtpClient:SmtpUrl"],Int32.Parse(_configuration["AppSetting:NotificationEmail:SmtpClient:SmtpPort"]),GetSecurity(_configuration["AppSetting:NotificationEmail:SmtpClient:SmtpSecure"]));
+                await client.AuthenticateAsync(_configuration["AppSetting:NotificationEmail:SmtpClient:SmtpLogin"], _configuration["AppSetting:NotificationEmail:SmtpClient:SmtpPassword"]);
                 await client.SendAsync(email);
                 await client.DisconnectAsync(true);
 
@@ -48,6 +46,15 @@ namespace SweetDebt.Service
                 return $"Bylo Ti ukradeno -{transaction.Amount} Kč.";
             else
                 return $"Bylo Ti vráceno {transaction.Amount} Kč.";
+        }
+        private MailKit.Security.SecureSocketOptions GetSecurity(string type)
+        {
+            if (type is null)
+                return MailKit.Security.SecureSocketOptions.None;
+            else
+            {
+                return type == "SSL" ? MailKit.Security.SecureSocketOptions.SslOnConnect : MailKit.Security.SecureSocketOptions.StartTls;
+            }
         }
     }
 }
